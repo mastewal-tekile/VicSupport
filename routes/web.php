a5a8b075526d57ce5cc00a21fe1aa8165c287330
mastewal-tekile <mastewaltekile@gmail.com>
2024-09-16 17:50:10 +0300

    route setted

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\TelegramBotController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/webhook',[TelegramBotController::class,'webhook']);
